440ac673143015a8479d6e2fd29bbb59dcb9c62d
Janith33 <janithchandupa6@gmail.com>
2026-06-29 20:35:00 +0530

    fixing the deployment problem

--- server.js
+++ server.js
@@ -1,45 +1,18 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
 const path = require("path");
 const { exec } = require("child_process");
 
+const app = express();
 const port = process.env.PORT || 3001;
 const rootDir = __dirname;
 
-const mimeTypes = {
-  ".html": "text/html; charset=utf-8",
-  ".css": "text/css; charset=utf-8",
-  ".js": "application/javascript; charset=utf-8",
-  ".json": "application/json; charset=utf-8",
-  ".jpg": "image/jpeg",
-  ".jpeg": "image/jpeg",
-  ".png": "image/png",
-  ".gif": "image/gif",
-  ".svg": "image/svg+xml",
-  ".ico": "image/x-icon",
-};
+app.use(express.static(rootDir));
 
-const server = http.createServer((req, res) => {
-  const requestPath = req.url === "/" ? "/index.html" : req.url;
-  const filePath = path.join(rootDir, decodeURIComponent(requestPath));
-  const ext = path.extname(filePath).toLowerCase();
-  const contentType = mimeTypes[ext] || "application/octet-stream";
-
-  fs.readFile(filePath, (error, content) => {
-    if (error) {
-      res.writeHead(error.code === "ENOENT" ? 404 : 500, {
-        "Content-Type": "text/plain; charset=utf-8",
-      });
-      res.end(error.code === "ENOENT" ? "Not Found" : "Server Error");
-      return;
-    }
-
-    res.writeHead(200, { "Content-Type": contentType });
-    res.end(content);
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(rootDir, "index.html"));
 });
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
   const url = `http://localhost:${port}`;
   const command =
@@ -55,3 +28,5 @@ server.listen(port, () => {
     }
   });
 });
+
+module.exports = app;
